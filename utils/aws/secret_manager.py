cca8b84f87e8b1654fb70ca6dfc065d3a532b0f5
abuawwadahmad <ahmad.hussein@quizplus.com>
2023-12-05 12:15:55 +0200

    Document reindexer

--- utils/aws/secret_manager.py
+++
@@ -1,9 +0,0 @@
-import boto3
-import json
-
-
-def get_secret(secret_name, secret_key):
-    client = boto3.client('secretsmanager')
-    response = client.get_secret_value(SecretId=secret_name)
-    secret_value = json.loads(response['SecretString'])
-    return secret_value[str(secret_key)]
